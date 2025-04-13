15158538fc8beba554542850c3420a543676de73
Raj <deadshot3r@pm.me>
2025-04-13 20:20:41 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import './globals.css'
 export const metadata: Metadata = {
   title: 'DeadShot | Portfolio',
   description: "Hi, It's me Raj or DeadShot. I am a Student, Graphics Designer, Coder and a Sleepy Nerd.",
-  icons: 'https://media.discordapp.net/attachments/1201570570765086780/1327591481837355068/deadshot-pfp.jpg?ex=67839f6a&is=67824dea&hm=a761fcd47ea6337e25927a9b4a3bf9ecf170caf53bf2e6fe268dd60d8847e786&=&format=webp&width=741&height=741',
+  icons: 'https://avatars.githubusercontent.com/u/63696670?v=4',
 }
 
 export default function RootLayout({
